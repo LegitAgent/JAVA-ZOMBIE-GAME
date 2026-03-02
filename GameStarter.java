d0488c15a59ebaa92265c3f4a8c9c81a32755f87
Alba Martin <albamartindarius@gmail.com>
2026-03-03 01:31:58 +0800

    fixed scaling problems with the singleplayer mode

--- GameStarter.java
+++ GameStarter.java
@@ -8,6 +8,7 @@ public class GameStarter {
      * Creates the game frame and connects to the GameServer
      */
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.uiScale", "1.0");
         GameFrame frame = new GameFrame();
         frame.connectToServer();
         frame.setUpGUI();
